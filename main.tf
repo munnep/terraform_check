d67393563226d65d610797670ac39d541839ef7b
munnep <patrick.munne@hashicorp.com>
2024-07-18 10:08:42 +0200

    test2

--- main.tf
+++ main.tf
@@ -4,6 +4,17 @@ resource "null_resource" "null_resource_foo" {
   }
 }
 
+check "health_check" {
+  data "http" "terraform_io" {
+    url = "https://www.terraform.io"
+  }
+
+  assert {
+    condition = data.http.terraform_io.status_code == 200
+    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+  }
+}
+
 # check "null_resource_name_check" {
 #   data "null_data_source" "null_data_source_bar" {
 #     inputs = {
