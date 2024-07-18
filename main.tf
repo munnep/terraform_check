f2bdcc19c7776b2585ed9aefc4a241cf11bda987
munnep <patrick.munne@hashicorp.com>
2024-07-18 10:27:54 +0200

    test4 wrong

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ check "health_check" {
   }
 
   assert {
-    condition = data.http.terraform_io.status_code == 200
+    condition = data.http.terraform_io.status_code == 500
     error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
   }
 }
