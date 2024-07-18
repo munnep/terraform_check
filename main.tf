37feb08fb455581189e2d2298fd340f118112860
munnep <patrick.munne@hashicorp.com>
2024-07-18 10:07:23 +0200

    tets

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ resource "null_resource" "null_resource_foo" {
   }
 }
 
-check "null_resource_name_check" {
-  data "null_data_source" "null_data_source_bar" {
-    inputs = {
-      bar = null_resource.null_resource_foo.triggers.foo
-    }
-  }
-  assert {
-    condition     = data.null_data_source.null_data_source_bar.outputs["bar"] == "oof"
-    error_message = "this should not fail"
-  }
-}
+# check "null_resource_name_check" {
+#   data "null_data_source" "null_data_source_bar" {
+#     inputs = {
+#       bar = null_resource.null_resource_foo.triggers.foo
+#     }
+#   }
+#   assert {
+#     condition     = data.null_data_source.null_data_source_bar.outputs["bar"] == "oof"
+#     error_message = "this should not fail"
+#   }
+# }
